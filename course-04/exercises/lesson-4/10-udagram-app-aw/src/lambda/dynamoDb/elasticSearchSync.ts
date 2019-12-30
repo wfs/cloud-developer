66eccbe961a5338a1fa21389a21e0b691757d8df
wfs <confirmalert@gmail.com>
2019-12-30 16:34:11 +1100

    Lesson4.9 Simple Notification done.

--- course-04/exercises/lesson-4/10-udagram-app-aw/src/lambda/dynamoDb/elasticSearchSync.ts
+++ course-04/exercises/lesson-4/10-udagram-app-aw/src/lambda/dynamoDb/elasticSearchSync.ts
@@ -1,14 +1,14 @@
 import { DynamoDBStreamEvent, DynamoDBStreamHandler } from "aws-lambda";
 import "source-map-support/register";
-// import * as elasticsearch from "elasticsearch";
-// import * as httpAwsEs from "http-aws-es";
+import * as elasticsearch from "elasticsearch";
+import * as httpAwsEs from "http-aws-es";
 
-// const esHost = process.env.ES_ENDPOINT;
+const esHost = process.env.ES_ENDPOINT;
 
-// const es = new elasticsearch.Client({
-//   hosts: [esHost],
-//   connectionClass: httpAwsEs
-// });
+const es = new elasticsearch.Client({
+  hosts: [esHost],
+  connectionClass: httpAwsEs
+});
 
 export const handler: DynamoDBStreamHandler = async (
   event: DynamoDBStreamEvent
@@ -17,28 +17,27 @@ export const handler: DynamoDBStreamHandler = async (
 
   for (const record of event.Records) {
     console.log("Processing record", JSON.stringify(record));
-    // if (record.eventName !== 'INSERT') {
-    //   continue
+    if (record.eventName !== "INSERT") {
+      continue;
+    }
+
+    const newItem = record.dynamodb.NewImage;
+
+    const imageId = newItem.imageId.S;
+
+    const body = {
+      imageId: newItem.imageId.S,
+      groupId: newItem.groupId.S,
+      imageUrl: newItem.imageUrl.S,
+      title: newItem.title.S,
+      timestamp: newItem.timestamp.S
+    };
+
+    await es.index({
+      index: "images-index",
+      type: "images",
+      id: imageId,
+      body
+    });
   }
-
-  // const newItem = record.dynamodb.NewImage
-
-  // const imageId = newItem.imageId.S
-
-  // const body = {
-  //   imageId: newItem.imageId.S,
-  //   groupId: newItem.groupId.S,
-  //   imageUrl: newItem.imageUrl.S,
-  //   title: newItem.title.S,
-  //   timestamp: newItem.timestamp.S
-  // }
-
-  // await es.index({
-  //   index: 'images-index',
-  //   type: 'images',
-  //   id: imageId,
-  //   body
-  // })
-
-  // }
 };
