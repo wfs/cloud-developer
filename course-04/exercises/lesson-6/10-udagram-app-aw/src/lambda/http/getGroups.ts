83e5a2ebd1f5eed4ea67d9d23c071c336ab04899
wfs <confirmalert@gmail.com>
2020-01-05 16:34:36 +1100

    Lesson 6 done but final exercise for course not working.

--- course-04/exercises/lesson-6/10-udagram-app-aw/src/lambda/http/getGroups.ts
+++ course-04/exercises/lesson-6/10-udagram-app-aw/src/lambda/http/getGroups.ts
@@ -1,13 +1,15 @@
 import "source-map-support/register";
-import { getAllGroups } from "../../businessLogic/groups";
+//import { getAllGroups } from "../../businessLogic/groups";
+import { GroupAccess } from "../../dataLayer/groupsAccess";
 
 import * as express from "express";
 import * as awsServerlessExpress from "aws-serverless-express";
 
 const app = express();
+const groupAccess = new GroupAccess();
 
 app.get("/groups", async (_req, res) => {
-  const groups = await getAllGroups();
+  const groups = await groupAccess.getAllGroups();
 
   res.json({
     items: groups
